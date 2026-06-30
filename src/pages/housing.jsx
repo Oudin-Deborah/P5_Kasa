be1638c1de779b3a358c08bf1264e2feb678ada5
Deborah Oudin <oudin.deborah.83@gmail.com>
2026-06-30 18:51:32 +0200

    last updates

--- src/pages/housing.jsx
+++ src/pages/housing.jsx
@@ -36,22 +36,22 @@ export default function Housing() {
       <Header />
       <main className="Housing__Main">
         <Slider pictures={data.pictures} />
-        <section className="Housing__Host">
-        <h2 className="Housing__Name">{data.title}</h2>
-        
-        <div className="Host">
-          <p className="Host__Name">{data.host.name}</p>
-          <img className="Host__img"></img>
-        </div>{" "}</section>
-        <p className="Housing__Location">{data.location}</p>
-        <div className="buttons">
-          {data.tags.map((tag) => (
-            <button className="Housing__button" key={tag}>
-              {tag}
-            </button>
-          ))}
-        </div>
-        <Rating rating={data.rating} />
+        <section className="Housing__Info">
+            <h2 className="Housing__Name">{data.title}</h2>
+            <div className="Host">
+              <p className="Host__Name">{data.host.name}</p>
+              <img className="Host__img"></img>
+            </div>{" "}
+          <p className="Housing__Location">{data.location}</p>
+          <div className="buttons">
+            {data.tags.map((tag) => (
+              <button className="Housing__button" key={tag}>
+                {tag}
+              </button>
+            ))}
+          </div>
+          <Rating rating={data.rating} />
+        </section>
         <div className="collapse__Description__Equipements">
           <Collapse className="Description" title="Description">
             <p>{data.description}</p>
